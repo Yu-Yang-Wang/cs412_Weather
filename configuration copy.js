adbf2650d084d55d05c7c55277a176680e5510fb
Yuyang Wang <uyang68@bu.edu>
2023-12-10 01:07:22 -0500

    ps4

---
+++ configuration copy.js
@@ -0,0 +1,5 @@
+module.exports = {
+    apiKey: 'f5296b222fmsh2f2c048197045efp1847d9jsn79a32789a432',
+    apiUrl: 'https://the-weather-api.p.rapidapi.com/api/weather/',
+    apiHost: 'the-weather-api.p.rapidapi.com'
+}
